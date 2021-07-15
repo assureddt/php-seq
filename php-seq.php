c6294a7f145ee8133d6105988819d4182d8a38d9
Dave Hamer <dave.hamer@assureddigitaltech.com>
2021-07-15 15:05:50 +0100

    fixed a few typos and changed post to postheaders. also the time field is required even though this apikey is set to use the server version

--- php-seq.php
+++ php-seq.php
@@ -6,13 +6,15 @@ $seq_server_url = "https://yourserverhere.com"; //can also include port number
 $api_key = ""; //if your seq server is set to enforce API keys, you should fill this in
 
 //log levels defined by SEQ are:
-
+//#TODO from SEQ doc
 
 //you should set $apploglevel at an application level and then pass it to this function
 //you should manually specify loglevel each time you log a message
 //this just passes a simple message to the log at a specific log level
 function log_to_seq_basic($apploglevel = "Informational", $loglevel = "Informational", $message)
 {
+	global $seq_server_url;
+	global $api_key;
 	//#TODO add loglevel
 	//#TODO compare loglevel with apploglevel
 
@@ -33,12 +35,12 @@ function log_to_seq_basic($apploglevel = "Informational", $loglevel = "Informati
 	$ch = curl_init();
 
 	$post_headers = [
-	    'ContentType' => 'application/vnd.serilog.clef',
-	    'X-Seq-ApiKey' => '$api_key',
+	    'ContentType: application/vnd.serilog.clef',
+	    'X-Seq-ApiKey: ' . $api_key,
 	];
 
 	//#TODO add extra log level and other features
-	$post_body = '{"@m":"$message"}';
+	$post_body = '{"@t":"'. date('Y-m-d H:i:s') .'","@m":"' . $message . '"}';
 
 	$ch = curl_init($seq_api_url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -49,7 +51,7 @@ function log_to_seq_basic($apploglevel = "Informational", $loglevel = "Informati
 
 	$response = curl_exec($ch);
 
-	if($curl_errno($ch))
+	if(curl_errno($ch))
 	{
 		$error_msg = curl_error($ch);
 	}
